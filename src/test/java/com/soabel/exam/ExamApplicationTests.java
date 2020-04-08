ac0b2d30786a72cb29b031ac82fcf327677fd4d2
soabel <alfredo.benaute@gmail.com>
2020-04-08 12:18:05 -0500

    feat -  load initial code

---
+++ src/test/java/com/soabel/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soabel.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
